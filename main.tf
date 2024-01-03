df9488e3563fc57f64bce70c4b61c6ccf9d531a8
hkawai <h.kawai.tech@gmail.com>
2024-01-03 23:34:55 +0900

    feat: VPC作成

--- main.tf
+++ main.tf
@@ -1,18 +1,31 @@
+# ---------------------------------------------
+# Terraform configuration
+# ---------------------------------------------
+terraform {
+  required_version = ">=0.13"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# ---------------------------------------------
+# Provider
+# ---------------------------------------------
 provider "aws" {
   profile = "terraform"
   region  = "ap-northeast-1"
 }
 
-resource "aws_instance" "hello-world" {
-  ami           = "ami-0dfa284c9d7b2adad"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "HelloWorld"
-  }
+# ---------------------------------------------
+# Variables
+# ---------------------------------------------
+variable "project" {
+  type = string
+}
 
-  user_data = <<EOF
-#!/bin/bash
-amazon-linux-extras install -y nginx1.12
-systemctl start nginx
-EOF
+variable "environment" {
+  type = string
 }
